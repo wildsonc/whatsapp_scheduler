b76ab65dc35e30c37576ca0a36a68495e74a4bb9
root <wil_753@hotmail.com>
2022-01-24 14:09:39 -0300

    init frontend

--- scheduler/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import HttpResponse
-from core.celery import debug_task_delay
-
-# Create your views here.
-def delay(request):
-    debug_task_delay.delay()
-    return HttpResponse()
